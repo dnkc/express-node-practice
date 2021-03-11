3e21b2d3d597b3b1e4dd069eaf1ab8af73160384
dnkc <dnkcdesign@gmail.com>
2021-03-10 22:54:15 -0500

    added user routes, validation, password encryption

--- models/userSchema.js
+++ models/userSchema.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -29,8 +30,22 @@ const userSchema = new mongoose.Schema({
         // must use function, not arrow function, to have access to "this"
         return el === this.password; // passes validation if passwords match. THESE ONLY WORKS ON SAVE (not update, must activate validators upon UPDATE)
       },
+      message: 'Passwords are not the same',
     },
   },
+});
+
+//password encryption
+// runs only if password is modified
+userSchema.pre('save', async function (next) {
+  if (!this.isModified('password')) return next();
+
+  // hash password with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+  //delete confirm password field from database
+  this.passwordConfirm = undefined; // only need password confirmation for signup validation, do not need to store it in the database
+  // it is required as input, but that does not mean it is required to be in the DB
+  next();
 });
 
 const User = mongoose.model('User', userSchema);
